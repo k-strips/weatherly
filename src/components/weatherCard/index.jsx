6d8abae220ecf78a71d32020851f65190b465a7d
k-strips <henblackstep@gmail.com>
2022-01-21 16:35:03 +0000

    [UPDATE] updated fetch hook, wired data to weather card

--- src/components/weatherCard/index.jsx
+++ src/components/weatherCard/index.jsx
@@ -2,49 +2,82 @@ import React, { useState, useEffect } from "react";
 import { useAxiosFetch } from "../../hooks";
 
 const WeatherCard = () => {
+  const [dataUrl, setDataUrl] = useState(null);
+  const [coordinate, setCoordinate] = useState({ lat: 0, lon: 0 });
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setCoordinate({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+        });
+      });
+    } else {
+      console.log(`geolocation not supported`);
+    }
+    // return () => {
+    //   setUrl("");
+    // };
+  }, []);
+
+  useEffect(() => {
+    setDataUrl(
+      `${process.env.REACT_APP_BASE_URL}?lat=${coordinate.lat}&lon=${coordinate.lon}&appid=${process.env.REACT_APP_API_KEY}`
+    );
+  }, [coordinate.lat, coordinate.lon]);
+
   // location state
-  const [location, setLocation] = useState("accra");
+  const [location, setLocation] = useState("");
   // input change handler for location
   const handleChange = (e) => {
     const { value } = e.target;
     setLocation(value);
   };
 
-  const cityNameUrl = `${process.env.REACT_APP_BASE_URL}?q=${location}&appid=${process.env.OPEN_WEATHER_API_KEY}`;
-
-  // coordinate state
-  const [coordinate, setCoordinate] = useState({ latitude: 0, longitude: 0 });
   useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setCoordinate({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        });
-      });
-    } else {
-      console.log(`geolocation not supported`);
-    }
+    doFetch(dataUrl);
+  }, [dataUrl]);
 
-    return () => {
-      setCoordinate({ latitude: 0, longitude: 0 });
-    };
-  }, []);
+  const [{ data, fetchError, isLoading }, doFetch] = useAxiosFetch(dataUrl);
 
-  const coordinateUrl = `${process.env.REACT_APP_BASE_URL}?lat=${coordinate.latitude}&lon=${coordinate.longitude}&appid=${process.env.REACT_APP_API_KEY}`;
+  // form on submit handler
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setDataUrl(
+      `${process.env.REACT_APP_BASE_URL}?q=${location}&appid=${process.env.REACT_APP_API_KEY}`
+    );
+  };
 
-  // const { data, fetchError, isLoading } = useAxiosFetch();
-
-  console.log(coordinateUrl);
+  console.log(data);
   return (
     <div className="flex flex-column flex-nowrap w-1/2">
       <h2 className="font-extrabold text-blue-600">weatherly</h2>
       <div className="flex flex-row flex-nowrap">
-        <input type="search" value={location} onChange={handleChange} />
-        <button>search</button>
+        <form onSubmit={handleSubmit}>
+          <input type="search" value={location} onChange={handleChange} />
+          <button type="submit">search</button>
+        </form>
       </div>
       <div className="flex flex-row flex-nowrap">
-        weather icons and details gos in here...
+        <div>
+          <figure>
+            <img
+              src={`https://openweathermap.org/img/wn/${data?.weather["icon"]}`}
+              alt="weather-icon"
+            />
+            <figcaption>{data?.weather?.description}</figcaption>
+          </figure>
+        </div>
+        <div>
+          {data && (
+            <>
+              <h3>
+                {data.name}, {data.sys.county}
+              </h3>
+              <h6>{new Date(data.dt).getDay()}</h6>
+            </>
+          )}
+        </div>
       </div>
       <div>icons here</div>
     </div>
--- src/hooks/useAxiosFetch.js
+++ src/hooks/useAxiosFetch.js
@@ -3,42 +3,27 @@ import { useState, useEffect } from "react";
 
 const useAxiosFetch = (dataUrl) => {
   const [data, setData] = useState(null);
+  const [url, setUrl] = useState(dataUrl);
   const [fetchError, setFetchError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    let isMounted = true;
-    const source = axios.CancelToken.source;
-
-    const fetchAsync = async (url) => {
+    const fetchAsync = async (params) => {
       setIsLoading(true);
       try {
-        const response = await axios.get(url, { cancelToken: source.token });
-        if (isMounted) {
-          setData(response.data);
-          setFetchError(null);
-        }
+        let response = await axios.request(url);
+        setData(response.data);
       } catch (error) {
-        if (isMounted) {
-          setFetchError(error.message);
-          setData([]);
-        }
+        setFetchError(error);
       } finally {
-        isMounted && setIsLoading(false);
+        setIsLoading(false);
       }
     };
 
-    fetchAsync(dataUrl);
+    fetchAsync();
+  }, [url]);
 
-    const cleanUp = () => {
-      isMounted = false;
-      source.cancel();
-    };
-
-    return cleanUp;
-  }, [dataUrl]);
-
-  return { data, fetchError, isLoading };
+  return [{ data, fetchError, isLoading }, setUrl];
 };
 
 export default useAxiosFetch;
